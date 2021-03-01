210209aa6772b0eb418a64691c27b21bb4146031
Johannes  Kruse <johanneskruse@Johanness-MacBook-Pro.local>
2021-03-01 16:20:46 +0100

    attention: token-to-token plot

--- visual_attention/bertviz/util.py
+++ visual_attention/bertviz/util.py
@@ -1,5 +1,11 @@
 import torch
 import ipywidgets as widgets
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 30, 'legend.fontsize': 20})
+plt.rc('font', size=25)
+plt.rc('axes', titlesize=25)
+
 
 def format_attention(attention):
     squeezed = []
@@ -13,14 +19,14 @@ def format_attention(attention):
     return torch.stack(squeezed)
 
 def format_special_chars(tokens):
-    return [t.replace('Ġ', ' ').replace('▁', ' ').replace('</w>', '') for t in tokens]
+    return [t.replace('Ġ', '').replace('▁', ' ').replace('</w>', '') for t in tokens]
 
 
 def drop_down(options, value=None, description='Select:', disabled=False):
     widget = widgets.Dropdown(    
         options=options,
-        value=None,
-        description='Select:',
+        value=value,
+        description=description,
         disabled=False,
     )
     return widget
@@ -65,3 +71,54 @@ def only_mask_attention(output_attention):
             zero_output_attention[i][ii][:-2]   = zero_output_attention[i][ii][:-2]*0
             zero_output_attention[i][ii][-1]    = zero_output_attention[i][ii][-1]*0
     return zero_output_attention
+
+
+def attention_token2token(tokens, attention, token1, token2):
+
+    for i, token in enumerate(tokens): 
+        if token == token1:
+            index_token1 = i
+        if token == token2:
+            index_token2 = i
+
+    attn_token2token = []
+    
+    for layer in attention:     
+        attention_temp = []
+        for head in layer:
+            attention_temp.append(head[index_token1, index_token2])
+        attn_token2token.append(attention_temp)
+
+    return attn_token2token
+
+
+def plot_attention_token2token(tokens, attention, token1, token2, color="blue"):
+
+    attention_scores = attention_token2token(tokens, attention, token1, token2)
+    attn_token2token_mean = [np.mean(attn_scores) for attn_scores in attention_scores]
+    
+    number_of_layers = len(attention_scores)
+    number_of_tokens = len(attention_scores[0])
+
+    # Begin Figure: 
+    figure, ax = plt.subplots(figsize=(12,9))
+
+
+    for i in range(number_of_layers): 
+        ax.plot([i]*number_of_tokens, attention_scores[i], ".", color=color)    
+    avg_attention, = ax.plot(range(len(attention_scores)), attn_token2token_mean, color=color)
+    avg_attention.set_label(f"[{token1}] $\longrightarrow$ [{token2}]")
+    ax.legend()
+
+    ax.set_title("Token-to-Token attention", size="x-large") 
+
+    ax.set_xlabel("Layer", fontsize="large")
+    ax.set_ylabel("Attention score", fontsize="large")
+
+    plt.tick_params(axis='x', labelsize="large")
+    plt.tick_params(axis='y', labelsize="large")
+    plt.grid()
+    plt.tight_layout()
+
+    return figure
+
